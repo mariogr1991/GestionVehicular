5e366423c4f5c08ff21eca98c05b4e9e1595e372
Programador2@Programador2-PC <mariogrcontacto@gmail.com>
2018-11-21 16:26:11 -0600

    Versión Estable

---
+++ MRCTools/CapturaError.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.IO;
+
+
+namespace MRCTools
+{
+    class CapturaError
+    {
+        public void Log(string mensaje)
+        {
+            string path = @"C:\Log\GestionVehicular\" + "Log" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " " + mensaje);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    AgregarLinea(mensaje, sw);
+                }
+            }
+
+        }
+
+        public  void AgregarLinea(string mensaje, TextWriter w)
+        {
+            w.WriteLine(DateTime.Now.ToString() + " " + mensaje);
+        }
+    }
+}
---
+++ MRCTools/Correo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading;
+using System.ComponentModel;
+using MRCTools;
+
+namespace MRCTools
+{
+    public class Correo
+    {
+        string host = "smtp.1and1.mx";
+        //string host = "smtp.gmail.com";        
+        int port = 587; //Es el mismo para gmail y el que se usa aquí.
+        NetworkCredential NW = new NetworkCredential("gestionvehicular@mrc.mx", "MrcGeVe2018");        
+
+        public string EnviarCorreo(string destinatario, string mensaje, string asunto)
+        {
+            try
+            {
+                SmtpClient cliente = new SmtpClient();
+                cliente.Host = host;
+                cliente.Port = port;
+                cliente.EnableSsl = true;
+                cliente.UseDefaultCredentials = false;
+                cliente.Credentials = NW;
+                MailAddress from = new MailAddress("gestionvehicular@mrc.mx", "Gestión Vehícular");
+                MailAddress to = new MailAddress(destinatario);
+                MailMessage mess = new MailMessage(from, to);                                
+                mess.Body = mensaje;                                         
+                mess.IsBodyHtml = true;
+                mess.Subject = asunto;                
+
+                cliente.Send(mess);
+                mess.Dispose();
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                CapturaError CE = new CapturaError();
+                CE.Log(e.Message + "Enviar Correo");
+                return "Error";
+            }
+        }
+    }
+}
